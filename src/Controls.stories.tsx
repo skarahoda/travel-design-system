62ee2a8bb9ce46327a48204fe6313109581e8595
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2021-10-12 11:28:15 +0300

    Add components to stories

--- src/Controls.stories.tsx
+++ src/Controls.stories.tsx
@@ -10,21 +10,33 @@ export default {
 };
 
 export const Tag = (args) => <TagComponent {...args}/>;
+Tag.parameters = {
+  component: TagComponent
+};
 Tag.args = {
   children: "Title"
 }
 
 export const Stepper = (args) => <StepperComponent {...args}/>;
+Stepper.parameters = {
+  component: StepperComponent
+};
 Stepper.args = {
   children: "Title"
 }
 
 export const Dropdown = (args) => <DropdownComponent {...args}/>;
+Dropdown.parameters = {
+  component: DropdownComponent
+};
 Dropdown.args = {
   children: "Title"
 }
 
 export const SearchBar = (args) => <SearchBarComponent {...args}/>;
+SearchBar.parameters = {
+  component: SearchBarComponent
+};
 SearchBar.args = {
   placeholder: "Search for a…"
 }
--- src/icons/Icon.stories.tsx
+++ src/icons/Icon.stories.tsx
@@ -25,13 +25,57 @@ export default {
 
 
 export const Profile = (args) => <ProfileIcon {...args} />;
+Profile.parameters = {
+  component: ProfileIcon
+};
+
 export const City = (args) => <CityIcon {...args} />;
+City.parameters = {
+  component: CityIcon
+};
+
 export const Guide = (args) => <GuideIcon {...args} />;
+Guide.parameters = {
+  component: GuideIcon
+};
+
 export const Hotel = (args) => <HotelIcon {...args} />;
+Hotel.parameters = {
+  component: HotelIcon
+};
+
 export const Restaurant = (args) => <RestaurantIcon {...args} />;
+Restaurant.parameters = {
+  component: RestaurantIcon
+};
+
 export const Weather = (args) => <WeatherIcon {...args} />;
+Weather.parameters = {
+  component: WeatherIcon
+};
+
 export const Time = (args) => <TimeIcon {...args} />;
+Time.parameters = {
+  component: TimeIcon
+};
+
 export const Plus = (args) => <PlusIcon {...args} />;
+Plus.parameters = {
+  component: PlusIcon
+};
+
 export const Minus = (args) => <MinusIcon {...args} />;
+Minus.parameters = {
+  component: MinusIcon
+};
+
 export const ArrowDown = (args) => <ArrowDownIcon {...args} />;
+ArrowDown.parameters = {
+  component: ArrowDownIcon
+};
+
 export const Search = (args) => <SearchIcon {...args} />;
+Search.parameters = {
+  component: SearchIcon
+};
+
