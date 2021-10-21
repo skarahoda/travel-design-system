5a21b07495e816d50be7e1cdf300292b0b6068eb
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2021-10-21 11:42:41 +0300

    Add color argument for icons

--- src/icons/Icon.stories.tsx
+++ src/icons/Icon.stories.tsx
@@ -33,51 +33,81 @@ Profile.parameters = {
 };
 
 export const City = (args) => <CityIcon {...args} />;
+City.args = {
+  color: IconColor.PINK
+};
 City.parameters = {
   component: CityIcon
 };
 
 export const Guide = (args) => <GuideIcon {...args} />;
+Guide.args = {
+  color: IconColor.PINK
+};
 Guide.parameters = {
   component: GuideIcon
 };
 
 export const Hotel = (args) => <HotelIcon {...args} />;
+Hotel.args = {
+  color: IconColor.PINK
+};
 Hotel.parameters = {
   component: HotelIcon
 };
 
 export const Restaurant = (args) => <RestaurantIcon {...args} />;
+Restaurant.args = {
+  color: IconColor.PINK
+};
 Restaurant.parameters = {
   component: RestaurantIcon
 };
 
 export const Weather = (args) => <WeatherIcon {...args} />;
+Weather.args = {
+  color: IconColor.PINK
+};
 Weather.parameters = {
   component: WeatherIcon
 };
 
 export const Time = (args) => <TimeIcon {...args} />;
+Time.args = {
+  color: IconColor.PINK
+};
 Time.parameters = {
   component: TimeIcon
 };
 
 export const Plus = (args) => <PlusIcon {...args} />;
+Plus.args = {
+  color: IconColor.PINK
+};
 Plus.parameters = {
   component: PlusIcon
 };
 
 export const Minus = (args) => <MinusIcon {...args} />;
+Minus.args = {
+  color: IconColor.PINK
+};
 Minus.parameters = {
   component: MinusIcon
 };
 
 export const ArrowDown = (args) => <ArrowDownIcon {...args} />;
+ArrowDown.args = {
+  color: IconColor.PINK
+};
 ArrowDown.parameters = {
   component: ArrowDownIcon
 };
 
 export const Search = (args) => <SearchIcon {...args} />;
+Search.args = {
+  color: IconColor.PINK
+};
 Search.parameters = {
   component: SearchIcon
 };
